e3195e34ae03404c479fc36ec7ece4d979497c12
GollaVijayBabu <gollavijaybabu221@gmail.com>
2021-11-18 08:40:20 -0800

    Added HashTable Program

--- Node.cs
+++ Node.cs
@@ -3,36 +3,36 @@ using System.Collections.Generic;
 
 namespace HashTable
 {
-    public class MyMapNode<K, V>
+    public class MyMapNode<G, V>
     {
         internal readonly int size;
-        private readonly LinkedList<KeyValue<K, V>>[] items;
+        private readonly LinkedList<KeyValue<G, V>>[] items;
 
         public MyMapNode(int size)
         {
             this.size = size;
-            this.items = new LinkedList<KeyValue<K, V>>[size];
+            this.items = new LinkedList<KeyValue<G, V>>[size];
         }
 
-        public struct KeyValue<k, v>
+        public struct KeyValue<G, v>
         {
-            public k Key { get; set; }
+            public G Key { get; set; }
             public v Value { get; set; }
         }
 
-        public void Add(K key, V value)
+        public void Add(G key, V value)
         {
             int position = GetArrayPosition(key);
-            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
-            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
+            LinkedList<KeyValue<G, V>> linkedList = GetLinkedList(position);
+            KeyValue<G, V> item = new KeyValue<G, V>() { Key = key, Value = value };
             linkedList.AddLast(item);
         }
 
-        public V Get(K key)
+        public V Get(G key)
         {
             int position = GetArrayPosition(key);
-            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
-            foreach (KeyValue<K, V> item in linkedList)
+            LinkedList<KeyValue<G, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<G, V> item in linkedList)
             {
                 if (item.Key.Equals(key))
                 {
@@ -42,30 +42,30 @@ namespace HashTable
             return default(V);
         }
 
-        protected int GetArrayPosition(K key)
+        protected int GetArrayPosition(G key)
         {
             int position = key.GetHashCode() % size;
             return Math.Abs(position);
         }
 
-        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        protected LinkedList<KeyValue<G, V>> GetLinkedList(int position)
         {
-            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            LinkedList<KeyValue<G, V>> linkedList = items[position];
             if (linkedList == null)
             {
-                linkedList = new LinkedList<KeyValue<K, V>>();
+                linkedList = new LinkedList<KeyValue<G, V>>();
                 items[position] = linkedList;
             }
             return linkedList;
         }
 
-        public void Remove(K key)
+        public void Remove(G key)
         {
             int position = GetArrayPosition(key);
-            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            LinkedList<KeyValue<G, V>> linkedList = GetLinkedList(position);
             bool itemFound = false;
-            KeyValue<K, V> foundItem = default(KeyValue<K, V>);
-            foreach (KeyValue<K, V> item in linkedList)
+            KeyValue<G, V> foundItem = default(KeyValue<G, V>);
+            foreach (KeyValue<G, V> item in linkedList)
             {
                 if (item.Key.Equals(key))
                 {
